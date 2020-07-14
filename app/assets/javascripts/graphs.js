181d1bbb7e6d540c7467d02d3bccb07cdbd5f9b0
kura-masahiro <dacanghongsheng@gmail.com>
2020-07-14 22:12:45 +0900

    railsからjavascriptにデータを渡しました

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,2 +1,42 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+document.addEventListener('turbolinks:load', () => {
+   let lineLabel = gon.chart_label
+   let lineData = gon.chart_data
+    
+
+
+    const lineChartData = {
+      labels: lineLabel,
+      datasets: [{
+        label: '体重(kg)',
+        data: lineData,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+        spanGaps: true
+      }]
+    }
+
+    const lineChartOption = {
+      title: {
+        display: true,
+        text: '折れ線グラフ'
+      },
+      tooltips: {
+        callbacks: {
+          title: function (tooltipItems) {
+            return tooltipItems[0].xLabel.replace(/^(\d+).(\d+)$/, ' $1 月 $2 日')
+          },
+          label: function (tooltipItem) {
+            return '体重: ' + tooltipItem.yLabel + 'kg'
+          }
+        }
+      }
+    }
+
+    const lineChartContext = document.getElementById("line-chart").getContext('2d')
+    new Chart(lineChartContext, {
+      type: 'line',
+      data: lineChartData,
+      options: lineChartOption
+    
+})
