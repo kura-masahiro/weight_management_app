d1de6d36eeba883e55a10e4869a604fcc884b8ed
kura-masahiro <dacanghongsheng@gmail.com>
2020-07-17 10:50:46 +0900

    各種機能、モーダルを作成しました

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,6 +1,57 @@
 document.addEventListener('turbolinks:load', () => {
-  
+   if (document.getElementById('start-calendar')) {
   const convertDate = (date) => new Date(new Date(date).setHours(0, 0, 0, 0))
+  const minDate = (date1, date2) => (date1 < date2) ? date1 : date2
+  const maxDate = (date1, date2) => (date1 > date2) ? date1 : date2
+
+  const START_DATE = convertDate(gon.weight_records[0].date)
+  const END_DATE = convertDate(gon.weight_records[gon.weight_records.length - 1].date)
+  
+  flatpickr.localize(flatpickr.l10ns.ja)
+
+  const drawGraphForPeriod = () => {
+    let from = convertDate(document.getElementById('start-calendar').value)
+    let to = convertDate(document.getElementById('end-calendar').value)
+
+    if (from > to) {
+      alert('終了日は開始日以降の日付に設定して下さい')
+    } else {
+      drawGraph(from, to)
+    }
+  }
+
+   const periodCalendarOption = {
+     disableMobile: true,
+     minDate: START_DATE,
+     maxDate: END_DATE,
+     onChange: drawGraphForPeriod
+   }
+
+   const startCalendarFlatpickr = flatpickr('#start-calendar', periodCalendarOption)
+   const endCalendarFlatpickr = flatpickr('#end-calendar', periodCalendarOption)
+
+    flatpickr('#new-calendar', {
+      disableMobile: true,
+      disable: gon.recorded_dates,
+      defaultDate: 'today',
+    })
+
+     const editCalendar = document.getElementById('edit-calendar')
+     const editWeight = document.getElementById('edit-weight')
+     const inputWeight = () => {
+       let record = gon.weight_records.find((record) => record.date === editCalendar.value)
+       editWeight.value = record.weight
+     }
+
+    
+     flatpickr('#edit-calendar', {
+       disableMobile: true,
+       enable: gon.recorded_dates,
+       noCalendar: gon.recorded_dates.length === 0,
+       onChange: inputWeight
+     })
+
+
 
   const TODAY = convertDate(new Date())
   const A_WEEK_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 6)
@@ -62,12 +113,36 @@ document.addEventListener('turbolinks:load', () => {
         options: weightOption
       })
     } else {
-     
       chartWeight.data = weightData
       chartWeight.options = weightOption
       chartWeight.update()
     }
   }
+  const drawGraphToToday = (from) => {
+  
+    from = maxDate(from, START_DATE)
+    let to = minDate(TODAY, END_DATE)
+    drawGraph(from, to)
+     startCalendarFlatpickr.setDate(from)
+     endCalendarFlatpickr.setDate(to)
+  }
 
-  drawGraph(A_WEEK_AGO, TODAY)
+  document.getElementById('a-week-button').addEventListener('click', () => {
+    drawGraphToToday(A_WEEK_AGO)
+  })
+
+  document.getElementById('two-weeks-button').addEventListener('click', () => {
+    drawGraphToToday(TWO_WEEKS_AGO)
+  })
+
+  document.getElementById('a-month-button').addEventListener('click', () => {
+    drawGraphToToday(A_MONTH_AGO)
+  })
+
+  document.getElementById('three-months-button').addEventListener('click', () => {
+    drawGraphToToday(THREE_MONTHS_AGO)
+  })
+
+drawGraphToToday(A_WEEK_AGO)
+   }
 })
